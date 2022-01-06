e00ba0ded977c771105648bf4afb6ea07e58ed90
Nakul Niroula <niroulanakul@gmail.com>
2022-01-06 20:07:23 +0545

    obstacle added

---
+++ src/game.js
@@ -0,0 +1,32 @@
+function Components(x, y, type, color) {
+  this.type = type;
+  this.color = color;
+  this.height = 500;
+  this.x = x;
+  this.y = y;
+  this.r = 20;
+  this.dx = 0;
+  this.dy = 0;
+  this.update = function () {
+    ctx = animationArea.context;
+    ctx.fillStyle = this.color;
+    if (this.type == "player") {
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+      ctx.stroke();
+      ctx.fill();
+    } else if (this.type == "opponents") {
+      ctx.beginPath();
+      ctx.arc(this.x + 100, this.y, this.r, 0, 2 * Math.PI);
+      ctx.stroke();
+      ctx.fill();
+    } else if (this.type == "obstacles") {
+      ctx.fillRect(this.x, this.y, 20, this.height);
+    } else if (this.type == "items") {
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, 10, 0, 2 * Math.PI);
+      ctx.stroke();
+      ctx.fill();
+    }
+  };
+}
---
+++ src/index.js
@@ -0,0 +1,37 @@
+let player;
+let opponents;
+let obstacles;
+let items;
+
+function startAnimation() {
+  player = new Components(100, 100, "player", "red");
+  opponents = new Components(200,100,"opponents", "blue");
+  obstacles = new Components(150,0,"obstacles", "black");
+  animationArea.start();
+}
+
+let animationArea = {
+  canvas: document.createElement("canvas"),
+  start: function () {
+    this.canvas.width = 1000;
+    this.canvas.height = 600;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    document.body.addEventListener("keydown", handleClick);
+
+    this.interval = setInterval(updateAnimationArea, 20);
+  },
+  clear: function () {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  },
+};
+
+function updateAnimationArea() {
+  animationArea.clear();
+  player.update();
+  opponents.update();
+  obstacles.update();
+}
+
+
+//handle click 
